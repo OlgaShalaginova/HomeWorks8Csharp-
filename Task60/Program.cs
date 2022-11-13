683f505108adffef2bb236bafea20720f14ac8d6
OlgaShalaginova <olechkatsapova@gmail.com>
2022-11-13 11:32:46 +0500

    task60

---
+++ Task60/Program.cs
@@ -0,0 +1,61 @@
+﻿// Сформируйте трёхмерный массив из случайных неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.Clear();
+int[,,] matrix = new int[2, 2, 2];
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]}{(i, j, k)} ");
+            } 
+            Console.WriteLine();   
+        }  
+    }
+}
+
+void FillMatrix(int[,,] matrix)
+{
+    Random rand = new Random();
+    
+    int[] temp = new int[matrix.GetLength(0) * matrix.GetLength(1) * matrix.GetLength(2)];
+    int  number;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = rand.Next(10, 100);
+        number = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = rand.Next(10, 100);
+                    j = 0;
+                    number = temp[i];
+                }
+                number = temp[i];
+            }
+        }
+    }
+    int count = 0; 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                matrix[i, j, k] = temp[count];
+                count++;
+            }
+        }
+    }
+}
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
